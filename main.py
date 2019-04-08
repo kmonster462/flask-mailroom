5b390f9897b0f5cd0994c5c4ca9e6de86704fb09
Kate Koehn <koehn.kathryn@gmail.com>
2019-04-07 20:28:47 -0700

    Committing hw1 files to repo

--- main.py
+++ main.py
@@ -3,19 +3,42 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor 
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
+
 
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+      
+    if request.method == 'POST':
+        try:
+            donor = Donor.select().where(Donor.name == request.form['donor']).get()
+            amount = request.form['donation']
+            Donation(donor=donor, value=amount).save()
+
+        except Donor.DoesNotExist:
+            # If donor doesn't exist, create a new donor with the donation amount indicated
+            new_donor = Donor(name=request.form['donor'])
+            new_donor.save()
+            Donation(donor=new_donor, value=request.form['donation']).save()
+        
+        return redirect(url_for('all')) 
+
+    else:
+        return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
